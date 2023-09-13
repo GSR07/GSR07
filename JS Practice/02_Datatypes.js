c1401b890d9a22311b1612bb13e0b644188f9418
Ganesh <42496810+GSR07@users.noreply.github.com>
2023-09-13 14:38:28 +0000

    javascript

---
+++ JS Practice/02_Datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) //we are using node js not an browser so that we should define it.
+
+//datatypes
+let name = "Ganesh" //string
+let age = 22 //integer
+let isLoggedIn = false //boolean
+
+// bigint -> used large integer value, used in stock market websites and all.
+// null datatype -> standalone value
+let city; //undefined
+console.log(isLoggedIn);
+
+// symbol -> to fine unique value in the code.
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
